992f2e8abf05b078adeb659c942eccb563e49b17
Ryan <rynpowers@gmail.com>
2019-02-05 14:30:45 -0500

    added try catch

--- server/routes/auth/auth.spec.js
+++ server/routes/auth/auth.spec.js
@@ -1,55 +1,79 @@
 const { expect } = require('chai');
 const { app } = require('../../');
 const request = require('supertest').agent(app);
-const { db, dbStore } = require('../../db');
+const { db } = require('../../db');
 
 const user = { email: 'ryn@email.com', password: 'password' };
 
 before(async () => {
-  await db.sync({ force: true });
+  try {
+    await db.sync({ force: true });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 describe('auth routes', () => {
   it('should fail to login a user without an account', async () => {
-    await request
-      .post('/auth/login')
-      .send(user)
-      .expect(401);
+    try {
+      await request
+        .post('/auth/login')
+        .send(user)
+        .expect(401);
+    } catch (err) {
+      console.log(err);
+    }
   });
 
   it('should signup a user', async () => {
-    await request
-      .post('/auth/signup')
-      .send(user)
-      .expect(200)
-      .then(res => expect(res.body.email).to.equal(user.email));
+    try {
+      await request
+        .post('/auth/signup')
+        .send(user)
+        .expect(200)
+        .then(res => expect(res.body.email).to.equal(user.email));
+    } catch (err) {
+      console.log(err);
+    }
   });
 
   it('should logout a user', async () => {
-    await request
-      .del('/auth/logout')
-      .send(user)
-      .expect(204);
+    try {
+      await request
+        .del('/auth/logout')
+        .send(user)
+        .expect(204);
+    } catch (err) {
+      console.log(err);
+    }
   });
 
   it('should not get a non logged in user', async () => {
-    await request
-      .get('/auth/me')
-      .send(user)
-      .expect(204);
+    try {
+      await request
+        .get('/auth/me')
+        .send(user)
+        .expect(204);
+    } catch (err) {
+      console.log(err);
+    }
   });
 
   it('should get a logged in user', async () => {
-    await request
-      .post('/auth/login')
-      .send(user)
-      .expect(200);
+    try {
+      await request
+        .post('/auth/login')
+        .send(user)
+        .expect(200);
 
-    await request
-      .get('/auth/me')
-      .expect(200)
-      .then(res => {
-        expect(res.body.email).to.equal(user.email);
-      });
+      await request
+        .get('/auth/me')
+        .expect(200)
+        .then(res => {
+          expect(res.body.email).to.equal(user.email);
+        });
+    } catch (err) {
+      console.log(err);
+    }
   });
 });
