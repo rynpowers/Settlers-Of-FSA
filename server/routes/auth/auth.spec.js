735a57892ccd4bafb2bbbf79847ab8a8205c1bed
Ryan <rynpowers@gmail.com>
2019-02-05 14:05:04 -0500

    server tests

---
+++ server/routes/auth/auth.spec.js
@@ -0,0 +1,55 @@
+const { expect } = require('chai');
+const { app } = require('../../');
+const request = require('supertest').agent(app);
+const { db, dbStore } = require('../../db');
+
+const user = { email: 'ryn@email.com', password: 'password' };
+
+before(async () => {
+  await db.sync({ force: true });
+});
+
+describe('auth routes', () => {
+  it('should fail to login a user without an account', async () => {
+    await request
+      .post('/auth/login')
+      .send(user)
+      .expect(401);
+  });
+
+  it('should signup a user', async () => {
+    await request
+      .post('/auth/signup')
+      .send(user)
+      .expect(200)
+      .then(res => expect(res.body.email).to.equal(user.email));
+  });
+
+  it('should logout a user', async () => {
+    await request
+      .del('/auth/logout')
+      .send(user)
+      .expect(204);
+  });
+
+  it('should not get a non logged in user', async () => {
+    await request
+      .get('/auth/me')
+      .send(user)
+      .expect(204);
+  });
+
+  it('should get a logged in user', async () => {
+    await request
+      .post('/auth/login')
+      .send(user)
+      .expect(200);
+
+    await request
+      .get('/auth/me')
+      .expect(200)
+      .then(res => {
+        expect(res.body.email).to.equal(user.email);
+      });
+  });
+});
---
+++ server/routes/auth/google.js
@@ -0,0 +1,46 @@
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const { User } = require('../../db');
+const passport = require('passport');
+const express = require('express');
+const router = express.Router();
+const chalk = require('chalk');
+const stratrgyConfig = {
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: process.env.GOOGLE_CALLBACK,
+};
+
+console.log(chalk.green('SETTEING UP OAUTH'));
+
+passport.use(
+  new GoogleStrategy(
+    stratrgyConfig,
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        const [user] = await User.findOrCreate({
+          where: { googleId: profile.id },
+          defaults: { email: profile.emails[0].value },
+        });
+        done(null, user);
+      } catch (err) {
+        done(err);
+      }
+    }
+  )
+);
+
+router.get(
+  '/',
+  passport.authenticate('google', { scope: ['profile', 'email'] })
+);
+
+router.get(
+  '/callback',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res, next) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  }
+);
+
+module.exports = router;
