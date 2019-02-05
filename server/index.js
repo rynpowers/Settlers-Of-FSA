e816c924661532ac8006676d62133fc36d6d7704
Ryan <rynpowers@gmail.com>
2019-02-05 15:53:50 -0500

    debugging supertest

--- server/index.js
+++ server/index.js
@@ -1,28 +1,15 @@
 const express = require('express');
 const app = express();
-const server = require('http').Server(app);
 const morgan = require('morgan');
 const { resolve } = require('path');
 const { db, User } = require('./db');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const dbStore = new SequelizeStore({ db: db });
-const io = require('socket.io')(server);
+const socketio = require('socket.io');
 const passport = require('passport');
 
-const port = process.env.PORT;
-
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    store: dbStore,
-    resave: false,
-    saveUninitialized: false,
-  })
-);
-
-app.use(passport.initialize());
-app.use(passport.session());
+const PORT = process.env.PORT || 3000;
 
 passport.serializeUser((user, done) => {
   try {
@@ -38,41 +25,65 @@ passport.deserializeUser((id, done) => {
     .catch(done);
 });
 
-process.env.NODE_ENV !== 'test' && app.use(morgan('dev'));
-app.use(express.static(resolve(__dirname, '../public')));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const createApp = () => {
+  app.use(morgan('dev'));
 
-app.use(require('./routes'));
+  app.use(express.static(resolve(__dirname, '../public')));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
 
-['/auth', '/api'].forEach(path => {
-  app.use(path, (req, res, next) => {
-    const err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET,
+      store: dbStore,
+      resave: false,
+      saveUninitialized: false,
+    })
+  );
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  app.use(require('./routes'));
+
+  ['/auth', '/api'].forEach(path => {
+    app.use(path, (req, res, next) => {
+      const err = new Error('Not Found');
+      err.status = 404;
+      next(err);
+    });
   });
-});
 
-app.use('*', (req, res, next) => {
-  res.sendFile(resolve(__dirname, '../public/index.html'));
-});
+  app.use('*', (req, res, next) => {
+    res.sendFile(resolve(__dirname, '../public/index.html'));
+  });
 
-app.use((err, req, res, next) => {
-  // console.log(err);
-  // console.error(err.stack);
-  res.status(err.status || 500).send(err.message || 'Internal Server Error');
-});
-
-const bootApp = async () => {
-  await db.sync();
-  await dbStore.sync();
-  server.listen(port, () => {
-    require('./socket.js')(io);
-    console.log('listening on port:', port);
+  app.use((err, req, res, next) => {
+    // console.log(err);
+    // console.error(err.stack);
+    res.status(err.status || 500).send(err.message || 'Internal Server Error');
   });
 };
 
+const startListening = () => {
+  // start listening (and create a 'server' object representing our server)
+  const server = app.listen(PORT, () =>
+    console.log(`Mixing it up on port ${PORT}`)
+  );
+  // set up our socket control center
+  const io = socketio(server);
+  require('./socket')(io);
+};
+
+const bootApp = async () => {
+  await dbStore.sync();
+  await db.sync();
+  await createApp();
+  startListening();
+};
+
 if (require.main === module) bootApp();
+else createApp();
 
 module.exports = {
   app,
