8b0d8eb61f4f5a54a0f07800cdcbb55382d605cf
Ryan <rynpowers@gmail.com>
2019-02-16 11:52:56 -0500

    completed visual representation of board

--- client/components/Board/index.js
+++ client/components/Board/index.js
@@ -1,39 +1,34 @@
 import React from 'react';
+import './index.scss';
 import ResourceList from './ResourceList';
 import options from './gameBoardOptions';
 
 const Board = () => {
   return (
-    <div
-      className="board-container"
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <ResourceList
-        num={3}
-        options={options.row1}
-        transform="translateY(60%)"
-      />
-      <ResourceList
-        num={4}
-        transform="translateY(30%)"
-        options={options.row2}
-      />
-      <ResourceList num={5} options={options.row3} />
-      <ResourceList
-        num={4}
-        options={options.row4}
-        transform="translateY(-30%)"
-      />
-      <ResourceList
-        num={3}
-        options={options.row5}
-        transform="translateY(-60%)"
-      />
+    <div className="board-container">
+      <div className="board-container-inner">
+        <ResourceList
+          num={3}
+          options={options.row1}
+          transform="translateY(60%)"
+        />
+        <ResourceList
+          num={4}
+          transform="translateY(30%)"
+          options={options.row2}
+        />
+        <ResourceList num={5} options={options.row3} />
+        <ResourceList
+          num={4}
+          options={options.row4}
+          transform="translateY(-30%)"
+        />
+        <ResourceList
+          num={3}
+          options={options.row5}
+          transform="translateY(-60%)"
+        />
+      </div>
     </div>
   );
 };
