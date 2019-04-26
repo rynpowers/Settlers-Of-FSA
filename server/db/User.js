d8c119510631faee77800a1c03fc01013b91369d
Ryan <rynpowers@gmail.com>
2019-04-26 16:08:02 -0400

    changed to bcrypt

--- server/db/User.js
+++ server/db/User.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 const db = require('./database');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 
 const User = db.define('users', {
   email: {
